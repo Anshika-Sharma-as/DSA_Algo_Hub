a09ed5256739a60d76f0317d99f5568ad952e5a7
vijaykanth_chennupati <vijaykanthch99@gmail.com>
2021-02-09 02:27:25 +0530

    linear search

--- linear_search.py
+++ linear_search.py
@@ -1,11 +1,19 @@
-import numpy as np
+"""
+best case  >>> O(1)
+worst case >>> O(n)
+"""
 
 def linear_search(data,find):
+
     for i in range(len(data)):
         if data[i]==find:
             print("found at position:",i)
         i+=1
     print("data is not present in the input_data")
-input_data=
+
+input_data=input("enter values(eg:-5 7 63 2):")
 find=int(input("enter the data to be search:"))
+input_data=input_data.split()
+for k in range(len(input_data)):
+    input_data[k]=int(input_data[k])
 linear_search(input_data,find)
