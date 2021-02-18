4bcd60599090ad0bb7ebd0495286fd5adb7e0cf2
vijaykanth_chennupati <vijaykanthch99@gmail.com>
2021-02-18 07:54:16 +0530

    bubble sort!

--- bubble_sort.py
+++ bubble_sort.py
@@ -1,9 +1,21 @@
+"""
+time complexity:
+---------------
+worst case >>> O(n2)  [because every is not in their sorted positions]
+best case  >>> O(n)   [because the input is sorted already so need to
+                       compare n elements n times again just one iteration
+                       is enough!]
+"""
 def bubble_sort(data):
     for t in range(len(data)):
+        trigger=0
         for i in range(len(data)-t-1):
             if data[i]>data[i+1]:
                 data[i],data[i+1]=data[i+1],data[i]
+                trigger=1
         i=0
+        if trigger==0:
+            break
     return data
 
 input_data=input("enter values(eg:-5 7 63 2):")
