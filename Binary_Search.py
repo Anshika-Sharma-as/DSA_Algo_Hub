98b51d3cc981cc2b1c30560e9ffcf0d605e6aa45
Yash Sankhla <78341485+yashsankhla21@users.noreply.github.com>
2021-02-26 13:15:32 +0530

    bin_search

---
+++ Binary_Search.py
@@ -0,0 +1,17 @@
+def binary_search(db,value):
+    start = 0
+    end = len(db)-1
+    middle = int((start+end)/2)
+    
+    while (db[middle]!=value and start<=end):
+        if value > db[middle]:
+            start=middle+1
+        else:
+            end = middle-1
+        middle=int((start+end)/2)
+        print("Start:",start , "End:",end,"Middle:",middle)
+    
+    if db[middle]==value:
+        print("we found at index",middle)
+    else:
+        print("Oops! Item Not Found")
